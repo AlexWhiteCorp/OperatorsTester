291c900cac652788648c2d701016a75fd2328e58
Alex White <47445536+AlexWhiteCorp@users.noreply.github.com>
2019-09-29 15:28:02 +0300

    Update HtmlTable.h

--- headers/HtmlTable.h
+++ headers/HtmlTable.h
@@ -1,1 +1,22 @@
+#pragma once
+#include <string>
+#include <fstream>
 
+using namespace std;
+
+class HtmlTable{
+public:
+    HtmlTable(string outputFileName, string fields[]);
+    void addNewRow(char operType, string varType, double time, double percent);
+    ~HtmlTable();
+
+private:
+    ofstream file;
+    bool isOpen = false;
+
+    void addNewCell(string value);
+    void addNewCell(char value);
+    void addNewCell(double value);
+    void addNewCell(double dv, string sv);
+    string percentToDiagram(double percent);
+};
